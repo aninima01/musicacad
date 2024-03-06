28f4330df7bb22d17ddcd579c20c99d97809e8c2
aninima01 <animabhattacharjee898@gmail.com>
2024-03-07 00:21:11 +0530

    added all files

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,12 @@
+import courseData from "../data/music_data.json";
+import React from 'react'
+
+function FeaturedCourses() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeaturedCourses
